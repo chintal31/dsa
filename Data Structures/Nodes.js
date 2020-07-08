6cc512e03be2ac7b09a4e90638a987aa9f4885ca
chintal31 <chinta3198@gmail.com>
2020-07-09 00:48:13 +0530

    removed comments from node file

--- Data Structures/Nodes.js
+++ Data Structures/Nodes.js
@@ -1,17 +1,8 @@
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null;
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-// class DoublyLinkedListNode{
-//     constructor(val){
-//         this.val = val;
-//         this.previous = null;
-//         this.next = null;
-//     }
-// }
-
-//module.exports ={ Node:Node,DoublyLinkedListNode };
 module.exports = Node;
